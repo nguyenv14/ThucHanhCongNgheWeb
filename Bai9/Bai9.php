f7e1b035c73e1083f1db3fffe5588ae25314c248
ThuDepGai <thule210903@gmail.com>
2022-08-25 11:48:58 +0700

    fourcommit

--- Bai9/Bai9.php
+++ Bai9/Bai9.php
@@ -4,22 +4,9 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Untitled Document</title>
 
-
-<?php
-    $file_name = $_FILES['ufile']['name'];
-    $file_size = $_FILES['ufile']['size'];
-    $file_temp = $_FILES['ufile']['tmp_name'];
-
-    move_uploaded_file($file_temp, "./image/".)
-
-
-?>
-
-
-
 </head>
 <body>
-<form action="" method="post" enctype="multipart/form-data" name="form1" id="form1">
+<form action="Bai9.php" method="post" enctype="multipart/form-data" name="form1" id="form1">
 <input type="hidden" name="MAX_FILE_SIZE" value="5000000"><!-- gioi han file upload - don vi la byte -->
 <label>Upload
 <input type="file" name="ufile" id="ufile" />
@@ -27,8 +14,35 @@
 <p>
 
 <input type="submit" name="upload" id="upload" value="Up load" />
-
 </p>
+<?php
+    
+    // move_uploaded_file($file_temp, "./image/".$file_name);
+    if(isset($_FILES['ufile'])){
+        $file_name = $_FILES['ufile']['name'];
+        $file_size = $_FILES['ufile']['size'];
+        $file_temp = $_FILES['ufile']['tmp_name'];
+        $taget_path = "image/".basename($file_name);
+        $regx = "/gif|jpg|png|jpeg/i";
+        if(!preg_match($regx, basename($file_name))){
+            echo "Đây không phải là file ảnh vui lòng upload lại";
+        }else{
+            if(file_exists($taget_path)){
+                echo "File ảnh đã tồn tại trong folder";
+            }else{
+                if($file_size > 20480000){
+                    echo "File ảnh quá lớn!!!";
+                }else{
+                    if(move_uploaded_file($file_temp, $taget_path)){
+                        echo "File ảnh ".$file_name." đã được upload!!!";
+                    }else{
+                        echo "Đã có lỗi khi upload file ảnh";
+                    }
+                }
+            }
+        }
+}
+?>
 </form>
 </body>
 </html>
